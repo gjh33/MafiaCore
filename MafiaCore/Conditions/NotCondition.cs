fb764768db05beb6ac4a7a489fb106b5ec539a27
gjh33 <manhappylife@yahoo.ca>
2021-09-13 22:08:08 -0700

    Add NotCondition. Needed for composite conditions and allows the use of NAND and NOR

---
+++ MafiaCore/Conditions/NotCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MafiaCore.Conditions
+{
+    public class NotCondition : Condition
+    {
+        public Condition Condition;
+
+        public override bool Evaluate(ExecutionParams context)
+        {
+            return !Condition.Evaluate(context);
+        }
+    }
+}
